7f63fdd585cf936e267abb5131856f1f76a1eccc
Rithvik Varma <rithvikvarmaa@gmail.com>
2023-12-06 17:22:05 -0500

    crew member class created

--- Employee.java
+++ Employee.java
@@ -46,3 +46,31 @@ class Manager extends Employee {
         return (getFixedSalary() / 22) * getDaysWorked();
     }
 }
+
+class CrewMember extends Employee {
+    private double hourlyPay;
+    private double hoursWorked;
+
+    public CrewMember(String name, int employeeId, double hourlyPay) {
+        super(name, employeeId);
+        this.hourlyPay = hourlyPay;
+        this.hoursWorked = 0;
+    }
+
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked += hoursWorked;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public double getHourlyPay() {
+        return hourlyPay;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return hourlyPay * hoursWorked;
+    }
+}
