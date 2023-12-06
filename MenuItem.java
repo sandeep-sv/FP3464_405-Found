30540156ddb1ee32c42af741145e619be19f1eb0
sandeep <itssandeepsv@gmail.com>
2023-12-06 14:09:57 -0500

    added MenuItem Class

---
+++ MenuItem.java
@@ -0,0 +1,25 @@
+class MenuItem {
+    private String itemName;
+    private double price;
+
+    public MenuItem(String itemName, double price) {
+        this.itemName = itemName;
+        this.price = price;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
