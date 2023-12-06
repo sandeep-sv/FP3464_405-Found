10a023f32524b827e93ffc267a8ca9f053aa1914
Rithvik Varma <rithvikvarmaa@gmail.com>
2023-12-06 17:21:18 -0500

    manager class created

--- Employee.java
+++ Employee.java
@@ -17,3 +17,32 @@ abstract class Employee implements SalaryCalculator {
     }
 
 }
+
+class Manager extends Employee {
+    private double fixedSalary;
+    private int daysWorked;
+
+    public Manager(String name, int employeeId, double fixedSalary) {
+        super(name, employeeId);
+        this.fixedSalary = fixedSalary;
+        this.daysWorked=0;
+    }
+
+    public int getDaysWorked() {
+        return daysWorked;
+    }
+
+    public double getFixedSalary() {
+        return fixedSalary;
+    }
+
+    public void setDaysWorked() {
+        this.daysWorked += 1;
+    }
+
+    @Override
+    public double calculateSalary() {
+        // Assuming a 22-day work month
+        return (getFixedSalary() / 22) * getDaysWorked();
+    }
+}
